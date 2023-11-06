ac0c3c184af675331fa691d57a37b9af58c077a7
Mahmoud <mahmoudalaa6868@gmail.com>
2023-11-07 00:23:50 +0200

    adding the Travel App project

--- src/assets/index.js
+++ src/assets/index.js
@@ -42,6 +42,7 @@ import HotelsReact from './projects/HotelsReact.png'
 import HotelsAngular from './projects/HotelsAngular.png'
 import TodoList from './projects/TodoList.png'
 import prayerTimes from './projects/prayerTimes.png'
+import TravelApp from './projects/TravelApp.png'
 
 import facebookIcon from "./icons/facebookIcon.png"
 import twitterIcon from "./icons/twitterIcon.png"
@@ -89,6 +90,7 @@ export {
   HotelsAngular,
   TodoList,
   prayerTimes,
+  TravelApp,
   facebookIcon,
   twitterIcon,
   githubIcon,

70a08af5329d8962207f18a49c06f1aca602e625
Mahmoud <mahmoudalaa6868@gmail.com>
2024-10-25 11:30:23 +0300

    latest update

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -97,7 +97,7 @@ const ProjectCard = ({
     <div>
       <Tilt
         options={{ max: 45, scale: 1, speed: 450 }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-[160px] h-full"
+        className="bg-tertiary p-2 sm:p-5 rounded-2xl sm:w-[360px] w-[160px] h-full"
       >
         <div className="relative w-full h-[150px] sm:h-[230px]">
           <img
